f3356066d3dfd11528370b300f47f808c8ec69f4
Mauro <mauro_gs@hotmail.com>
2015-02-08 20:52:43 +0100

    Added ratings to movies

--- Rails/movie_review/config/routes.rb
+++ Rails/movie_review/config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+
   devise_for :users
-  resources :movies
+  resources :movies do
+    resources :reviews, except: [:show, :index]
+  end
 
   root 'movies#index'
+  
 end
